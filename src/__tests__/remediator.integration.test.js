3aaedb5620a5ed37932363fcc9f24561baac7524
Jason Zajdel <zajdel.jason@gmail.com>
2018-09-10 11:52:39 -0700

    Updated to perform building and validating of config/options.

--- src/__tests__/remediator.integration.test.js
+++ src/__tests__/remediator.integration.test.js
@@ -1,9 +1,36 @@
 import remediator from '../index';
+import {
+  OUTPUT_DIRECTORY_KEY,
+  SOURCE_DIRECTORIES_KEY,
+  RECURSIVE_KEY,
+  FORMAT_KEY,
+  DEFAULT_FORMAT,
+  DEFAULT_RECURSIVE,
+} from '../constants';
+import { MissingOptionError } from '../errors';
 
-describe('src/index.js', () => {
-  test('remediator', () => {
-    const result = remediator();
+describe('src integration tests', () => {
+  test('should throw error when required options not passed in', () => {
+    expect.assertions(1);
+    return expect(remediator())
+      .rejects
+      .toEqual(new MissingOptionError(OUTPUT_DIRECTORY_KEY));
+  });
 
-    expect(result).toEqual(true);
+  test('should successfully execute using default config values', () => {
+    const options = {
+      [OUTPUT_DIRECTORY_KEY]: './foo',
+      [SOURCE_DIRECTORIES_KEY]: './bar',
+    };
+    const expected = {
+      ...options,
+      [FORMAT_KEY]: DEFAULT_FORMAT,
+      [RECURSIVE_KEY]: DEFAULT_RECURSIVE,
+    };
+
+    expect.assertions(1);
+    return expect(remediator(options))
+      .resolves
+      .toEqual(expected);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export default function remediator() {
-  return true;
+import buildConfig from './buildConfig';
+import validateConfig from './validateConfig';
+
+/**
+ * Reorganizes media files based off meta data.
+ * @param   {object} options Remediator options object.
+ * @returns {object}         Returns config object (temporary).
+ */
+export default async function remediator(options = {}) {
+  const config = buildConfig(options);
+  await validateConfig(config);
+
+  return config;
 }
