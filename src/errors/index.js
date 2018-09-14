b2fbb426d810f551e3fa410483c33212bac1450f
Jason Zajdel <zajdel.jason@gmail.com>
2018-09-14 14:39:02 -0700

    Removed Error that no longer made sense

--- src/errors/index.js
+++ src/errors/index.js
@@ -2,4 +2,3 @@ export { default as MissingOptionError } from './MissingOptionError';
 export { default as InvalidOptionError } from './InvalidOptionError';
 export { default as InvalidOutputDirectory } from './InvalidOutputDirectory';
 export { default as InvalidSourceDirectory } from './InvalidSourceDirectory';
-export { default as NoOutputAsSource } from './NoOutputAsSource';
