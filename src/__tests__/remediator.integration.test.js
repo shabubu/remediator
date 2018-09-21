52a45ad3032ff06d990db8e375528eebcd82af20
Jason Zajdel <zajdel.jason@gmail.com>
2018-09-20 23:14:21 -0700

    Now retrieving all files from config sources, updated integration tests

--- src/__tests__/remediator.integration.test.js
+++ src/__tests__/remediator.integration.test.js
@@ -5,53 +5,37 @@ import {
   SOURCE_DIRECTORIES_KEY,
   RECURSIVE_KEY,
   FORMAT_KEY,
-  DEFAULT_RECURSIVE,
-  TRANSFORMER_ACTIONS_KEY,
-  TRANSFORMER_NAME_KEY,
-  TRANSFORMER_REPLACE_STRING_KEY,
-  MODE_KEY,
-  MODE_COPY,
 } from 'src/constants';
 import { MissingOptionError } from 'src/errors';
 
 describe('Remediator integration tests', () => {
-  test('should throw error when required options not passed in', () => {
+  test('should reject error when required options not passed in', () => {
     expect.assertions(1);
     return expect(remediator())
       .rejects
       .toEqual(new MissingOptionError(OUTPUT_DIRECTORY_KEY));
   });
 
-  test('should successfully execute using default config values', () => {
-    const validDirectory = '../';
+  test('should successfully resolve using default config values', () => {
+    const sourceDir = './testAssets';
+    const outputDir = './testAssets';
     const options = {
-      [OUTPUT_DIRECTORY_KEY]: validDirectory,
-      [SOURCE_DIRECTORIES_KEY]: validDirectory,
+      [OUTPUT_DIRECTORY_KEY]: sourceDir,
+      [SOURCE_DIRECTORIES_KEY]: outputDir,
       [FORMAT_KEY]: ': Day :.:Ext:',
+      [RECURSIVE_KEY]: true,
     };
-    const expected = {
-      ...options,
-      [OUTPUT_DIRECTORY_KEY]: path.resolve(validDirectory),
-      [SOURCE_DIRECTORIES_KEY]: [
-        path.resolve(validDirectory),
-      ],
-      [RECURSIVE_KEY]: DEFAULT_RECURSIVE,
-      [MODE_KEY]: MODE_COPY,
-      [TRANSFORMER_ACTIONS_KEY]: [
-        {
-          [TRANSFORMER_NAME_KEY]: 'Day',
-          [TRANSFORMER_REPLACE_STRING_KEY]: ': Day :',
-        },
-        {
-          [TRANSFORMER_NAME_KEY]: 'Ext',
-          [TRANSFORMER_REPLACE_STRING_KEY]: ':Ext:',
-        },
-      ],
-    };
+    const expected = [
+      path.resolve(sourceDir, '1.jpg'),
+      path.resolve(sourceDir, '2.jpg'),
+      path.resolve(sourceDir, 'subDir/3.jpg'),
+      path.resolve(sourceDir, 'subDir2/4.jpg'),
+      path.resolve(sourceDir, 'subDir/anotherSubDir/6.jpg'),
+    ];
 
     expect.assertions(1);
     return expect(remediator(options))
       .resolves
-      .toEqual(expected);
+      .toEqual(expected.sort());
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import buildConfig from 'src/lib/buildConfig';
+import { getFilesFromDirectories } from 'src/lib/fsHelpers';
+import {
+  RECURSIVE_KEY,
+  SOURCE_DIRECTORIES_KEY,
+} from 'src/constants';
 
 /**
  * Reorganizes media files based off meta data.
- * @param   {object} options Remediator options object.
- * @returns {object}         Returns config object (temporary).
+ * @param    {object}  options Remediator options object.
+ * @returns  {Promise}
+ * @resolves {Array}           Resolves array of source files (temporary).
  */
-export default function remediator(options = {}) {
-  const config = buildConfig(options);
+export default async function remediator(options = {}) {
+  const config = await buildConfig(options);
+  const sourceFiles = await getFilesFromDirectories(
+    config[SOURCE_DIRECTORIES_KEY],
+    config[RECURSIVE_KEY],
+  );
 
-  return config;
+  return sourceFiles;
 }
