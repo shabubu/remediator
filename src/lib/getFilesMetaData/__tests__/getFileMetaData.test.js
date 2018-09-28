859f9d6b7457af12ece78a5cdde331401ab86040
Jason Zajdel <zajdel.jason@gmail.com>
2018-09-27 21:48:52 -0700

    Added submodule that builds a files remediator metadata object

---
+++ src/lib/getFilesMetaData/__tests__/getFileMetaData.test.js
@@ -0,0 +1,70 @@
+import path from 'path';
+import { SKIP_ERRORS_KEY } from 'src/constants';
+import {
+  LIB_DATE_KEY,
+  LIB_ERROR_KEY,
+  LIB_HEIGHT_KEY,
+  LIB_MAKE_KEY,
+  LIB_MODEL_KEY,
+  LIB_ORIENTATION_KEY,
+  LIB_ORIGINAL_FILE_PATH,
+  LIB_WIDTH_KEY,
+} from 'src/constants/metaData';
+import {
+  closeExifToolProcess,
+  openExifToolProcess,
+} from 'src/lib/exifTool';
+import getFileMetaData from 'src/lib/getFilesMetaData/getFileMetaData';
+
+describe('src/lib/getFilesMetaData/getFileMetaData', () => {
+  // setup and tear down exiftool process for test(s)
+  beforeEach(done => openExifToolProcess().then(() => done()));
+  afterEach(() => closeExifToolProcess());
+
+  test('should resolve object of metadata for a media file', () => {
+    const config = {};
+    const filePath = path.resolve('./testAssets/1.jpg');
+    const expected = {
+      [LIB_DATE_KEY]: 946717200000,
+      [LIB_HEIGHT_KEY]: '1',
+      [LIB_WIDTH_KEY]: '1',
+      [LIB_MODEL_KEY]: 'A Camera Model',
+      [LIB_MAKE_KEY]: 'A Camera Maker',
+      [LIB_ORIGINAL_FILE_PATH]: filePath,
+      [LIB_ORIENTATION_KEY]: 'Horizontal (normal)',
+    };
+    const result = getFileMetaData(config, filePath);
+
+    expect.assertions(1);
+    return expect(result)
+      .resolves
+      .toEqual(expected);
+  });
+
+  test('should resolve error when skip errors is true', () => {
+    const config = {
+      [SKIP_ERRORS_KEY]: true,
+    };
+    const filePath = path.resolve('./testAssets/notAFile.jpg');
+    const expected = {
+      [LIB_ERROR_KEY]: expect.anything(),
+    };
+    const result = getFileMetaData(config, filePath);
+
+    expect.assertions(1);
+    return expect(result)
+      .resolves
+      .toEqual(expected);
+  });
+
+  test('should throw if error encountered', () => {
+    const config = {};
+    const filePath = path.resolve('./testAssets/notAFile.jpg');
+    const result = getFileMetaData(config, filePath);
+
+    expect.assertions(1);
+    return expect(result)
+      .rejects
+      .toThrow();
+  });
+});
---
+++ src/lib/getFilesMetaData/getFileMetaData.js
@@ -0,0 +1,57 @@
+import { SKIP_ERRORS_KEY } from 'src/constants';
+import {
+  EXIF_JPG_DEVICE_MAKE_KEY,
+  EXIF_JPG_DEVICE_MODEL_KEY,
+  EXIF_JPG_ORIENTATION_KEY,
+  EXIF_JPG_RESOLUTION,
+  LIB_DATE_KEY,
+  LIB_ERROR_KEY,
+  LIB_HEIGHT_KEY,
+  LIB_MAKE_KEY,
+  LIB_MODEL_KEY,
+  LIB_ORIENTATION_KEY,
+  LIB_ORIGINAL_FILE_PATH,
+  LIB_WIDTH_KEY,
+} from 'src/constants/metaData';
+import getExifToolMetaData from 'src/lib/getFilesMetaData/getExifToolMetaData';
+import getFileSystemMetaData from 'src/lib/getFilesMetaData/getFileSystemMetaData';
+import getOldestTimestamp from 'src/lib/getFilesMetaData/getOldestTimestamp';
+
+/**
+ * Gets the remediator meta data for transformers.
+ * @param    {object}  config   Remediator config object.
+ * @param    {string}  filePath File to build metadata from.
+ * @returns  {Promise}
+ * @resolves {object}           Object of metadata of object with error if skipping errors and error
+ *                              encountered.
+ * @rejects  {Error}            Rejects an error if not skipping errors and error encountered.
+ */
+export default async function getFileMetaData(config, filePath) {
+  try {
+    const fsMeta = await getFileSystemMetaData(filePath);
+    const exifMeta = await getExifToolMetaData(filePath);
+    const [
+      width,
+      height,
+    ] = exifMeta[EXIF_JPG_RESOLUTION].split('x');
+
+    return {
+      [LIB_DATE_KEY]: getOldestTimestamp(fsMeta, exifMeta),
+      [LIB_HEIGHT_KEY]: height,
+      [LIB_MAKE_KEY]: exifMeta[EXIF_JPG_DEVICE_MAKE_KEY],
+      [LIB_MODEL_KEY]: exifMeta[EXIF_JPG_DEVICE_MODEL_KEY],
+      [LIB_ORIENTATION_KEY]: exifMeta[EXIF_JPG_ORIENTATION_KEY],
+      [LIB_ORIGINAL_FILE_PATH]: filePath,
+      [LIB_WIDTH_KEY]: width,
+    };
+  } catch (error) {
+    // return error in object if we are skipping errors
+    if (config[SKIP_ERRORS_KEY]) {
+      return {
+        [LIB_ERROR_KEY]: error,
+      };
+    }
+
+    throw error;
+  }
+}
