cc796a7b9d3851de079e00c09529ffdbff4f158b
Jason Zajdel <zajdel.jason@gmail.com>
2018-09-29 11:29:12 -0700

    Fixed error in docs

--- src/lib/transformPaths/transformers/getNumericDay.js
+++ src/lib/transformPaths/transformers/getNumericDay.js
@@ -3,7 +3,7 @@ import { LIB_DATE_KEY } from 'src/constants/metaData';
 /**
  * Generates numeric day from meta data.
  * @param   {object} fileMetaData Remediator meta data object.
- * @returns {number}              Returns numeric day of month.
+ * @returns {number}              Returns numeric day of day.
  */
 export default function getNumericDay(fileMetaData) {
   return fileMetaData[LIB_DATE_KEY].getDate();
--- src/lib/transformPaths/transformers/getNumericDayWithLeadingZero.js
+++ src/lib/transformPaths/transformers/getNumericDayWithLeadingZero.js
@@ -4,7 +4,7 @@ import { prependZero } from 'src/lib/util';
 /**
  *  Generates numeric day with leading zero from meta data.
  * @param   {object} fileMetaData Remediator meta data object.
- * @returns {number}              Returns numeric day of month with leading zero.
+ * @returns {number}              Returns numeric day of day with leading zero.
  */
 export default function getNumericDayWithLeadingZero(fileMetaData) {
   return prependZero(getNumericDay(fileMetaData));
