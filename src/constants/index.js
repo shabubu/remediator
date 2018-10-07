1b63f4e5979e7a10d23de6b281a480c2059f39df
Jason Zajdel <zajdel.jason@gmail.com>
2018-10-06 23:08:16 -0700

    Added constants for return file object keys

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export * from 'src/constants/config';
 export * from 'src/constants/transformers';
 export * from 'src/constants/metaData';
+export * from 'src/constants/outputData';
---
+++ src/constants/outputData.js
@@ -0,0 +1,11 @@
+/**
+ * Return value for a transformed files' new path.
+ * @type {string}
+ */
+export const RETURN_DATA_OUTPUT_KEY = 'output';
+
+/**
+ * Return value for a transormed files' original path.
+ * @type {string}
+ */
+export const RETURN_DATA_SOURCE_KEY = 'source';
