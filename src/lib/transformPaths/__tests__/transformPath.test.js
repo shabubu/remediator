99113eb9b4a8b8e1b6014332e83939fd102dbab9
Jason Zajdel <zajdel.jason@gmail.com>
2018-10-06 23:09:11 -0700

    Added submodule that transforms metadata into a output filepath

---
+++ src/lib/transformPaths/__tests__/transformPath.test.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import {
+  FORMAT_KEY,
+  OUTPUT_DIRECTORY_KEY,
+  RETURN_DATA_OUTPUT_KEY,
+  RETURN_DATA_SOURCE_KEY,
+  SOURCE_DIRECTORIES_KEY,
+} from 'src/constants';
+import buildConfig from 'src/lib/buildConfig';
+import getFileMetaData from 'src/lib/getFilesMetaData/getFileMetaData';
+import transformPath from 'src/lib/transformPaths/transformPath';
+
+describe('src/lib/transformPaths/transformPath', () => {
+  test('should return object with original file path and transformed path', async () => {
+    const output = './';
+    const source = './testAssets';
+    const config = await buildConfig({
+      [OUTPUT_DIRECTORY_KEY]: output,
+      [SOURCE_DIRECTORIES_KEY]: source,
+    });
+    const filePath = path.join(config[SOURCE_DIRECTORIES_KEY][0], '1.jpg');
+    const metaData = await getFileMetaData(config, filePath);
+    const expected = {
+      [RETURN_DATA_OUTPUT_KEY]: path.join(
+        config[OUTPUT_DIRECTORY_KEY],
+        '2000/01. January/01 Saturday/2000.01.01 01.00.jpg',
+      ),
+      [RETURN_DATA_SOURCE_KEY]: filePath,
+    };
+    const result = transformPath(config, metaData);
+
+    expect.assertions(1);
+    expect(result).toEqual(expected);
+  });
+
+  test('should return object with transformed path that had a empty transformer', async () => {
+    const output = './';
+    const source = './testAssets';
+    const config = await buildConfig({
+      [OUTPUT_DIRECTORY_KEY]: output,
+      [SOURCE_DIRECTORIES_KEY]: source,
+      [FORMAT_KEY]: ':YYYY::-Model-:.:Ext:',
+    });
+    const filePath = path.join(config[SOURCE_DIRECTORIES_KEY][0], '2.jpg');
+    const metaData = await getFileMetaData(config, filePath);
+    const expected = {
+      [RETURN_DATA_OUTPUT_KEY]: path.join(
+        config[OUTPUT_DIRECTORY_KEY],
+        '2000.jpg',
+      ),
+      [RETURN_DATA_SOURCE_KEY]: filePath,
+    };
+    const result = transformPath(config, metaData);
+
+    expect.assertions(1);
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/lib/transformPaths/transformPath.js
@@ -0,0 +1,39 @@
+import path from 'path';
+import {
+  FORMAT_KEY,
+  LIB_ORIGINAL_FILE_PATH,
+  OUTPUT_DIRECTORY_KEY,
+  RETURN_DATA_OUTPUT_KEY,
+  RETURN_DATA_SOURCE_KEY,
+  TRANSFORMER_ACTIONS_KEY,
+  TRANSFORMER_NAME_KEY,
+  TRANSFORMER_REPLACE_STRING_KEY,
+} from 'src/constants';
+import transformers from 'src/lib/transformPaths/transformers';
+
+/**
+ * "Transforms" file meta data into new file path.
+ * @param   {object} config       Remediator config object.
+ * @param   {object} fileMetaData File Remediator metadata object.
+ * @returns {object}                   Returns object with source filename and transformed output.
+ */
+export default function transformPath(config, fileMetaData) {
+  let newPath = config[FORMAT_KEY];
+  const transformerActions = config[TRANSFORMER_ACTIONS_KEY];
+
+  transformerActions.forEach((action) => {
+    const transformer = action[TRANSFORMER_NAME_KEY];
+    const transformResult = transformers[transformer](fileMetaData);
+    const replace = transformResult || '';
+
+    newPath = newPath.replace(
+      action[TRANSFORMER_REPLACE_STRING_KEY],
+      replace,
+    );
+  });
+
+  return {
+    [RETURN_DATA_OUTPUT_KEY]: path.join(config[OUTPUT_DIRECTORY_KEY], newPath),
+    [RETURN_DATA_SOURCE_KEY]: fileMetaData[LIB_ORIGINAL_FILE_PATH],
+  };
+}
