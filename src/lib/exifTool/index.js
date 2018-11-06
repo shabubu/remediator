d067c19085b5db488a5167628968a0ad33ebe360
Jason Zajdel <zajdel.jason@gmail.com>
2018-11-05 20:14:34 -0800

    Dropping old module for exiftool in preparation for new dependency.  Moving mock to new name.

--- src/lib/exifTool/index.js
+++
@@ -1,3 +0,0 @@
-export { default as closeExifToolProcess } from 'src/lib/exifTool/closeExifToolProcess';
-export { default as getExifToolInstance } from 'src/lib/exifTool/getExifToolInstance';
-export { default as openExifToolProcess } from 'src/lib/exifTool/openExifToolProcess';
