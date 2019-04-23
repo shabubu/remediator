ee9dda5390151ca880668e5b7d6af9c657d6ec64
Jason Zajdel <zajdel.jason@gmail.com>
2019-04-23 13:40:27 -0700

    Added transformer constants for locations

---
+++ src/constants/transformers/location.js
@@ -0,0 +1,70 @@
+/**
+ * City name transformer.
+ * @type {string}
+ */
+export const CITY = 'City';
+
+/**
+ * Lowercase city name transformer.
+ * @type {string}
+ */
+export const CITY_LOWERCASE = 'lcCity';
+
+/**
+ * Uppercase city name transformer.
+ * @type {string}
+ */
+export const CITY_UPPERCASE = 'ucCity';
+
+/**
+ * Country name transformer.
+ * @type {string}
+ */
+export const COUNTRY = 'Country';
+
+/**
+ * Lowercase country name transformer.
+ * @type {string}
+ */
+export const COUNTRY_LOWERCASE = 'lcCountry';
+
+/**
+ * Uppercase country name transformer.
+ * @type {string}
+ */
+export const COUNTRY_UPPERCASE = 'ucCountry';
+/**
+ * State name transformer.
+ * @type {string}
+ */
+export const STATE = 'State';
+
+/**
+ * Lowercase state name transformer.
+ * @type {string}
+ */
+export const STATE_LOWERCASE = 'lcState';
+
+/**
+ * Uppercase state name transformer.
+ * @type {string}
+ */
+export const STATE_UPPERCASE = 'ucState';
+
+/**
+ * Street name transformer.
+ * @type {string}
+ */
+export const STREET = 'Street';
+
+/**
+ * Lowercase street name transformer.
+ * @type {string}
+ */
+export const STREET_LOWERCASE = 'lcStreet';
+
+/**
+ * Uppercase street name transformer.
+ * @type {string}
+ */
+export const STREET_UPPERCASE = 'ucStreet';
