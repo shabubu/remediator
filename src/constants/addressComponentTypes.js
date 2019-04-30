c6d79009759746a0bd4670e3012de344c83eab4a
Jason Zajdel <zajdel.jason@gmail.com>
2019-04-29 23:01:16 -0700

    Added constants for address component object keys

---
+++ src/constants/addressComponentTypes.js
@@ -0,0 +1,35 @@
+/**
+ * Street number google component key.
+ * @type {string}
+ */
+export const STREET_NUMBER = 'street_number';
+
+/**
+ * Street name google component key.
+ * @type {string}
+ */
+export const STREET_NAME = 'route';
+
+/**
+ * City google component key.
+ * @type {string}
+ */
+export const CITY = 'locality';
+
+/**
+ * Street number google component key.
+ * @type {string}
+ */
+export const STATE = 'administrative_area_level_1';
+
+/**
+ * Country google component key.
+ * @type {string}
+ */
+export const COUNTRY = 'country';
+
+/**
+ * Postal code google component key.
+ * @type {string}
+ */
+export const POSTAL_CODE = 'postal_code';
